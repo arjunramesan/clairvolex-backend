9bc50a34627ebcd13f37b11e7c9bf0647bac4825
arjunramesan01 <arjun.ramesan@byjus.com>
2024-03-23 18:46:47 +0530

    init

--- database.js
+++ database.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'clairvolex_db',
-  password: 'ultram@N1996'
+  host: process.env.HOST, 
+  user: process.env.USER,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD 
 });
 
 module.exports = pool;
